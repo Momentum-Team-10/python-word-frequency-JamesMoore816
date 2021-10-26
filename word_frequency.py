b28e0e15b10774f04e827bc45e38c92048eeff94
James Moore <james816am@gmail.com>
2021-10-26 19:11:48 -0400

    Everything works as intended, includes comments

--- word_frequency.py
+++ word_frequency.py
@@ -25,17 +25,20 @@ def print_word_freq(file):
         text_string = text_string.lower()
         # Takes the text string and turns it back into a list, which should be only words and no punctuation
         word_list = text_string.split()
-        # Removes any words contained in STOP_WORDS from the list of words
-        print(word_list)
+        # Creates a new list of words that have filtered out all stop words
+        word_list_filtered = []
+        for word in word_list:
+            if word not in STOP_WORDS:
+                word_list_filtered.append(word)
         # This list variable is initialized so we can append strings to it in the following for loop
         word_count_list = []
-        # In this loop, the count method is called on word_list to count instances of a word, and saved to the
-        # word_count variable as a string. The list created above has an entry appended that includes the
-        # word, number of times it appears, and asterisks equal to that number. This list contains duplicate
-        # entries for each time the word appears, and is not sorted.
-        for word in word_list:
-            word_count = str(word_list.count(word))
-            word_count_list.append(word + " | " + word_count + " " + ('*' * int(word_count)))
+        # In this loop, the count method is called on word_list_filtered to count instances of a word, and
+        # saved to the word_count variable as a string. The list created above has an entry appended that
+        # includes the word, number of times it appears, and asterisks equal to that number. This list
+        # contains duplicate entries for each time the word appears, and is not sorted.
+        for word in word_list_filtered:
+            word_count = str(word_list_filtered.count(word))
+            word_count_list.append(f"{word} | {word_count} {'*' * int(word_count)}")
         # Sorts the list by frequency count in reverse order, so the most frequent entries are listed first
         word_count_list = sorted(word_count_list, key=word_count_list.count, reverse=True)
         # Duplicates are removed by converting the list to a set, which may not contain duplicates
@@ -43,8 +46,8 @@ def print_word_freq(file):
         # The set is sorted using the index of its respective list as a key, as that list was already sorted
         # by frequency
         word_count_no_dupes = sorted(word_count_no_dupes, key=word_count_list.index)
-        # for item in word_count_no_dupes:
-        #     print(item)
+        for item in word_count_no_dupes:
+            print(item)
             
 
 
